540a800d19d1e3cd106a6f0e5d487d330c33be86
Boris Kozak <boriskozak@gmail.com>
2020-11-04 15:56:21 -0500

    test integration

--- src/test.js
+++ src/test.js
@@ -5,6 +5,13 @@ module.exports = {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-    }
+    },
+
+    needsBirth: function(cat) {
+        if (cat[0] == true && cat[1] == true) {
+            return true;
+        } else {
+            return false;
+    }}
 
 };
