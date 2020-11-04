72580c313c6c0a7b529b4b987e15268279125b6f
Boris Kozak <boriskozak@gmail.com>
2020-11-04 15:52:28 -0500

    test integration

---
+++ src/test.js
@@ -0,0 +1,10 @@
+// Helper functions
+
+module.exports = {
+    getRandomIntInclusive: function(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    }
+
+};
